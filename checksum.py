a97df84d246529d5327b7b78578b7adc75f2dd15
Thomas Rizzo <rizzot@oregonstate.edu>
2025-10-21 13:48:06 -0700

    Need to write function to compute checksum now

--- checksum.py
+++ checksum.py
@@ -1,10 +1,12 @@
 NUM_FILES = 10
 
 def getIPaddrs(file):
-    with open(file, "r") as f: #changed from rb to r
-        src_addr, dest_addr = f.read().decode('utf-8').split(" ")
+    with open(file, "r") as f:
+        src_addr, dest_addr = f.read().split(" ")
+        dest_addr = dest_addr.strip() # Remove \n from dest_addr
     return src_addr, dest_addr
 
+# Can probably just modify this to take two IP arguments and combine with getIPaddrs to reduce # of lines of code
 def pack_ip(ip):
     unpack_bytes = ip.split(".")
     byte_arr = bytearray()
@@ -16,14 +18,30 @@ def createPsuedoIPHeader(src, dest, tcp_len):
     byte_arr = bytearray()
     zero = b'\x00'
     ptcl = b'\x06'
-    return ptcl
+    components = [src, dest, zero, ptcl, tcp_len]
+    for component in components:
+        byte_arr.append(component)
+    return byte_arr
 
-def GetTCPLength(file):
+def processTCPData(file):
     with open(file, "rb") as fp:
         tcp_data = fp.read()
+        extracted_checksum = tcp_data[16:18]
         tcp_length = len(tcp_data)
-        return tcp_length
+        gen_new_zero_cksum_tcp = tcp_data[:16] + b'\x00\x00' + tcp_data[18:]
+        if len(gen_new_zero_cksum_tcp) % 2 == 0:
+            gen_new_zero_cksum_tcp += b'\x00'
+        return tcp_length, extracted_checksum, gen_new_zero_cksum_tcp
 
+def main():
+    src, dest = getIPaddrs("tcp_data/tcp_addrs_0.txt")
+    src_bytes, dest_bytes = pack_ip(src), pack_ip(dest)
+    tcp_len, extracted_cksum, zero_cksum_tcp = processTCPData("tcp_data/tcp_data_0.dat")
+    ipPsuedoHeader = createPsuedoIPHeader(src_bytes, dest_bytes, tcp_len)
+    return
+
+if __name__ == "__main__":
+    main()
 
 
 # Read in the tcp_addrs_0.txt file.
